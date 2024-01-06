562c52b9ffd7fceecb02401f43ee82fa92304e66
Pruthvi K P <117765336+pruthvikp@users.noreply.github.com>
2024-01-06 17:07:38 +0530

    Update insurance.sql

--- insurance.sql
+++ insurance.sql
@@ -96,7 +96,63 @@ INSERT INTO PARTICIPATED VALUES
 
 
 SELECT *FROM PERSON;
++-----------+--------+----------------+
+| driver_id | name   | address        |
++-----------+--------+----------------+
+| 01AB11    | Smith  | Mysore         |
+| 01AB12    | Suresh | Bengaluru      |
+| 01AB13    | Ramesh | Chikkamagaluru |
+| 01AB14    | Viraj  | Tumakuru       |
+| 01AB15    | Soumya | Mandya         |
++-----------+--------+----------------+
+5 rows in set (0.01 sec)
+
 SELECT *FROM CAR;
++------------+----------+------+
+| regno      | model    | year |
++------------+----------+------+
+| KA09MA1111 | HARRIER  | 2019 |
+| KA09MA1234 | NEXON    | 2020 |
+| KA09MB1212 | MERCEDES | 2020 |
+| KA09MC5656 | MAZDA    | 2021 |
+| KA09MD3434 | BMW      | 2018 |
++------------+----------+------+
+5 rows in set (0.00 sec)
+
 SELECT *FROM ACCIDENT;
++---------------+------------+-----------+
+| report_number | acc_date   | location  |
++---------------+------------+-----------+
+|          1021 | 2021-01-01 | Mysuru    |
+|          1022 | 2021-02-01 | Bengaluru |
+|          1023 | 2021-03-01 | Mumbai    |
+|          1024 | 2022-01-01 | Chennai   |
+|          1025 | 2023-01-01 | Bengaluru |
++---------------+------------+-----------+
+5 rows in set (0.00 sec)
+
 SELECT *FROM OWNS;
++-----------+------------+
+| driver_id | regno      |
++-----------+------------+
+| 01AB11    | KA09MC5656 |
+| 01AB11    | KA09MA1111 |
+| 01AB13    | KA09MA1234 |
+| 01AB14    | KA09MB1212 |
+| 01AB15    | KA09MD3434 |
++-----------+------------+
+5 rows in set (0.00 sec)
+
 SELECT *FROM PARTICIPATED;
++-----------+------------+---------------+---------------+
+| driver_id | regno      | report_number | damage_amount |
++-----------+------------+---------------+---------------+
+| 01AB11    | KA09MC5656 |          1021 |         30000 |
+| 01AB11    | KA09MA1111 |          1022 |         40000 |
+| 01AB13    | KA09MA1234 |          1023 |         50000 |
+| 01AB14    | KA09MB1212 |          1024 |         60000 |
+| 01AB15    | KA09MD3434 |          1025 |        700000 |
++-----------+------------+---------------+---------------+
+5 rows in set (0.00 sec)
+
+
