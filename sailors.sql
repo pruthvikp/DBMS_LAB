8a7817323332a717560fbae743e9451d77783978
Pruthvi K P <117765336+pruthvikp@users.noreply.github.com>
2024-01-06 20:56:04 +0530

    Update sailors.sql

--- sailors.sql
+++ sailors.sql
@@ -67,34 +67,158 @@ INSERT INTO RESERVES VALUES
 (3,102,"2024-02-05"),
 (4,103,"2024-02-06");
 
+SELECT *FROM SAILORS;  
+/*
++-----+------------------+--------+-----+
+| sid | sname            | rating | age |
++-----+------------------+--------+-----+
+|   1 | Albert Ullagaddi |    9.8 |  20 |
+|   2 | Ramesh Shetty    |      8 |  30 |
+|   3 | Supriya Dodmani  |    7.8 |  40 |
+|   4 | Storm Smith      |    5.2 |  45 |
+|   5 | Warner Storm     |    6.5 |  52 |
+|   6 | John Storms      |    8.2 |  56 |
++-----+------------------+--------+-----+
+6 rows in set (0.00 sec)
+*/
+
+SELECT *FROM BOAT;
+/*
++-----+--------------+-------+
+| bid | bname        | color |
++-----+--------------+-------+
+| 101 | Vikramaditya | Blue  |
+| 102 | Titanic      | White |
+| 103 | Vikranth     | Red   |
++-----+--------------+-------+
+3 rows in set (0.00 sec)
+*/
+  
+SELECT *FROM RESERVES;
+/*
++-----+-----+------------+
+| sid | bid | rdate      |
++-----+-----+------------+
+|   1 | 101 | 2024-02-01 |
+|   1 | 102 | 2024-02-02 |
+|   1 | 103 | 2024-02-03 |
+|   2 | 101 | 2024-02-04 |
+|   3 | 102 | 2024-02-05 |
+|   4 | 103 | 2024-02-06 |
++-----+-----+------------+
+6 rows in set (0.00 sec)
+*/
+
 -- Find the colours of boats reserved by Albert
 SELECT b.color 
 FROM SAILORS s, BOAT b, RESERVES r
 WHERE s.sid=r.sid AND b.bid=r.bid AND s.sname LIKE "%Albert%";
-
+/*
++-------+
+| color |
++-------+
+| Blue  |
+| White |
+| Red   |
++-------+
+3 rows in set (0.00 sec)
+*/
+  
 -- Find all sailor id's of sailors who have a rating of at lease 8 or reserved boat 103
 (SELECT sid FROM SAILORS WHERE rating>=8)
 UNION 
 (SELECT sid FROM RESERVES WHERE bid=103);
-
+/*
++-----+
+| sid |
++-----+
+|   1 |
+|   2 |
+|   6 |
+|   4 |
++-----+
+4 rows in set (0.01 sec)
+*/
+  
 (SELECT sid,sname FROM SAILORS WHERE rating>=8)
 UNION 
 (SELECT r.sid,s.sname FROM SAILORS s, BOAT b, RESERVES r WHERE  s.sid=r.sid AND b.bid=r.bid and r.bid=103);
-
+/*
++-----+------------------+
+| sid | sname            |
++-----+------------------+
+|   1 | Albert Ullagaddi |
+|   2 | Ramesh Shetty    |
+|   6 | John Storms      |
+|   4 | Storm Smith      |
++-----+------------------+
+4 rows in set (0.00 sec)
+*/
+  
 -- Find the name of the sailors who have not reserved a boat whose name contains the string 'storm'. Order the names in ascending order.
 SELECT s.sname FROM SAILORS s
 WHERE s.sid NOT IN
 (SELECT s1.sid FROM SAILORS s1, RESERVES r1 WHERE s1.sid=r1.sid AND s1.sname LIKE "%storm%")
 AND s.sname LIKE "%storm%"
 ORDER BY s.sname ASC;
+/*
++--------------+
+| sname        |
++--------------+
+| John Storms  |
+| Warner Storm |
++--------------+
+2 rows in set (0.00 sec)
+*/
+  
+-- Find the names of sailors who have reserved all boats.
+SELECT sname FROM SAILORS s WHERE NOT EXISTS
+(SELECT * FROM BOAT b WHERE NOT EXISTS
+(SELECT * FROM RESERVES r WHERE s.sid=r.sid and b.bid=r.bid));
+/*
++------------------+
+| sname            |
++------------------+
+| Albert Ullagaddi |
++------------------+
+1 row in set (0.00 sec)
+*/
+
+-- Find name and age of the oldest sailor
+SELECT sname,age FROM SAILORS WHERE age IN (SELECT max(AGE) FROM SAILORS);
+/*
++-------------+-----+
+| sname       | age |
++-------------+-----+
+| John Storms |  56 |
++-------------+-----+
+1 row in set (0.00 sec)
+*/
 
 -- Find the names of sailors who have reserved all boats.
 SELECT sname FROM SAILORS s WHERE NOT EXISTS
 (SELECT * FROM BOAT b WHERE NOT EXISTS
 (SELECT * FROM RESERVES r WHERE s.sid=r.sid and b.bid=r.bid));
+/*
++------------------+
+| sname            |
++------------------+
+| Albert Ullagaddi |
++------------------+
+1 row in set (0.00 sec)
+*/
 
 -- Find name and age of the oldest sailor
 SELECT sname,age FROM SAILORS WHERE age IN (SELECT max(AGE) FROM SAILORS);
+/*
++-------------+-----+
+| sname       | age |
++-------------+-----+
+| John Storms |  56 |
++-------------+-----+
+1 row in set (0.00 sec)
+*/
+
 
 -- Find each boat which was reserved by atleast 2 sailors with age>=40,
 --  find the boat id and the average age of such sailors.
@@ -102,6 +226,16 @@ SELECT r.bid, avg(s.age) AS average_age
 FROM SAILORS s, BOAT b, RESERVES r
 WHERE s.sid=r.sid AND b.bid=r.bid AND s.age>=40
 GROUP BY b.bid HAVING COUNT(r.sid)>=2;
+/*
++-----+-------------+
+| bid | average_age |
++-----+-------------+
+| 101 |     45.0000 |
+| 102 |     44.0000 |
+| 103 |     46.5000 |
++-----+-------------+
+3 rows in set (0.01 sec)
+*/
 
 -- Create a view that shows the names and colours of all the boats that have been reserved by a sailor with a specific rating.
 CREATE VIEW ReservedBoatByRating AS
@@ -128,5 +262,5 @@ END;
 // DELIMITER ;
 
 DELETE FROM BOAT WHERE bid=103;
-
+-- Error 1645: Boat is reserved and hence cannot be deleted
 
