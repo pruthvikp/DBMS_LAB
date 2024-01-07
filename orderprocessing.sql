e7c9155e740c83ad78790516f5f34380ecb5228d
Pruthvi K P <117765336+pruthvikp@users.noreply.github.com>
2024-01-07 10:00:02 +0530

    Update orderprocessing.sql

--- orderprocessing.sql
+++ orderprocessing.sql
@@ -69,25 +69,33 @@ foreign key (warehouse_id) references WAREHOUSE(warehouse_id) on delete cascade
 );
 
 INSERT INTO CUSTOMER VALUES
-(0001,'Sia','Mysuru'),
-(0002,'Mia','Bengaluru'),
-(0003,'Pia','Mumbai'),
-(0004,'Kumar','Delhi'),
-(0005,'Tia','Chennai');
+(1,'Sia','Mysuru'),
+(2,'Mia','Bengaluru'),
+(3,'Pia','Mumbai'),
+(4,'Kumar','Delhi'),
+(5,'Tia','Chennai');
 
 INSERT INTO ORDERS VALUES
-(1001, "2020-01-14", 0001, 2000),
-(1002, "2021-04-13", 0002, 500),
-(1003, "2019-10-02", 0003, 2500),
-(1004, "2019-05-12", 0005, 1000),
-(1005, "2020-12-23", 0004, 1200);
+(1001, "2020-01-14", 1, 2000),
+(1002, "2021-04-13", 2, 500),
+(1003, "2019-10-02", 3, 2500),
+(1004, "2019-05-12", 5, 1000),
+(1005, "2020-12-23", 4, 1200),
+(1006, "2021-12-12", 3, 5000);
 
 INSERT INTO ITEM VALUES
-(0001, 400),
-(0002, 200),
-(0003, 1000),
-(0004, 100),
-(0005, 500);
+(10001, 400),
+(10002, 200),
+(10003, 1000),
+(10004, 100),
+(10005, 500);
+
+INSERT INTO ORDER_ITEM VALUES 
+(1001, 10001, 5),
+(1002, 10005, 1),
+(1003, 10005, 5),
+(1004, 10003, 1),
+(1005, 10004, 12);
 
 INSERT INTO WAREHOUSE VALUES
 (501, "Mysuru"),
@@ -96,16 +104,243 @@ INSERT INTO WAREHOUSE VALUES
 (504, "Dehli"),
 (505, "Chennai");
 
-INSERT INTO ORDER_ITEM VALUES 
-(1001, 0001, 5),
-(1002, 0005, 1),
-(1003, 0005, 5),
-(1004, 0003, 1),
-(1005, 0004, 12);
-
 INSERT INTO SHIPMENT VALUES
-(1001, 0002, "2020-01-16"),
-(1002, 0001, "2021-04-14"),
-(1003, 0004, "2019-10-07"),
-(1004, 0003, "2019-05-16"),
-(1005, 0005, "2020-12-23");
+(1001, 501, "2020-01-16"),
+(1002, 502, "2021-04-14"),
+(1003, 502, "2019-10-07"),
+(1004, 504, "2019-05-16"),
+(1005, 505, "2020-12-23");
+
+SELECT * FROM CUSTOMER;
+/*+---------+-------+-----------+
+| cust_id | cname | city      |
++---------+-------+-----------+
+|       1 | Sia   | Mysuru    |
+|       2 | Mia   | Bengaluru |
+|       3 | Pia   | Mumbai    |
+|       4 | Kumar | Delhi     |
+|       5 | Tia   | Chennai   |
++---------+-------+-----------+
+5 rows in set (0.00 sec)
+*/
+
+SELECT * FROM ORDERS;
+/*
++----------+------------+---------+-----------+
+| order_id | odate      | cust_id | order_amt |
++----------+------------+---------+-----------+
+|     1001 | 2020-01-14 |       1 |      2000 |
+|     1002 | 2021-04-13 |       2 |       500 |
+|     1003 | 2019-10-02 |       3 |      2500 |
+|     1004 | 2019-05-12 |       5 |      1000 |
+|     1005 | 2020-12-23 |       4 |      1200 |
+|     1006 | 2021-12-12 |       3 |      5000 |
++----------+------------+---------+-----------+
+6 rows in set (0.00 sec)
+*/
+
+SELECT * FROM ITEM;
+/*
++---------+-----------+
+| item_id | unitprice |
++---------+-----------+
+|   10001 |       400 |
+|   10002 |       200 |
+|   10003 |      1000 |
+|   10004 |       100 |
+|   10005 |       500 |
++---------+-----------+
+5 rows in set (0.00 sec)
+*/
+
+SELECT * FROM ORDER_ITEM;
+/*
++----------+---------+-----+
+| order_id | item_id | qty |
++----------+---------+-----+
+|     1001 |   10001 |   5 |
+|     1002 |   10005 |   1 |
+|     1003 |   10005 |   5 |
+|     1004 |   10003 |   1 |
+|     1005 |   10004 |  12 |
++----------+---------+-----+
+5 rows in set (0.00 sec)
+*/
+
+SELECT * FROM WAREHOUSE;
+/*
++--------------+-----------+
+| warehouse_id | city      |
++--------------+-----------+
+|          501 | Mysuru    |
+|          502 | Bengaluru |
+|          503 | Mumbai    |
+|          504 | Dehli     |
+|          505 | Chennai   |
++--------------+-----------+
+5 rows in set (0.00 sec)
+*/
+
+SELECT * FROM SHIPMENT;
+/*
++----------+--------------+------------+
+| order_id | warehouse_id | ship_date  |
++----------+--------------+------------+
+|     1001 |          501 | 2020-01-16 |
+|     1002 |          502 | 2021-04-14 |
+|     1003 |          502 | 2019-10-07 |
+|     1004 |          504 | 2019-05-16 |
+|     1005 |          505 | 2020-12-23 |
++----------+--------------+------------+
+5 rows in set (0.00 sec)
+*/
+
+-- List the Order# and Ship_date for all orders shipped from Warehouse# "W2" (warehouse_id=502).
+SELECT order_id,ship_date
+FROM SHIPMENT
+WHERE warehouse_id=502;
+/*
++----------+------------+
+| order_id | ship_date  |
++----------+------------+
+|     1002 | 2021-04-14 |
+|     1003 | 2019-10-07 |
++----------+------------+
+2 rows in set (0.00 sec)
+*/
+
+-- List the Warehouse information from which the Customer named "Kumar" was supplied his
+-- orders. Produce a listing of Order#, Warehouse#.
+SELECT s.order_id,s.warehouse_id 
+FROM SHIPMENT s,ORDERS o,CUSTOMER c
+WHERE cname LIKE '%kumar%' AND c.cust_id=o.cust_id AND o.order_id=s.order_id;
+/*
++----------+--------------+
+| order_id | warehouse_id |
++----------+--------------+
+|     1005 |          505 |
++----------+--------------+
+1 row in set (0.00 sec)
+*/
+
+-- Produce a listing: Cname, #ofOrders, Avg_Order_Amt, where the middle column is the total
+-- number of orders by the customer and the last column is the average order amount for that
+-- customer. (Use aggregate functions)
+SELECT cname,COUNT(order_id),AVG(order_amt) 
+FROM CUSTOMER c,ORDERS o
+WHERE c.cust_id=o.cust_id
+GROUP BY cname;
+/*
++-------+-----------------+----------------+
+| cname | COUNT(order_id) | AVG(order_amt) |
++-------+-----------------+----------------+
+| Sia   |               1 |      2000.0000 |
+| Mia   |               1 |       500.0000 |
+| Pia   |               2 |      3750.0000 |
+| Kumar |               1 |      1200.0000 |
+| Tia   |               1 |      1000.0000 |
++-------+-----------------+----------------+
+5 rows in set (0.00 sec)
+*/
+
+-- Delete all orders for customer named "Kumar".
+DELETE FROM ORDERS 
+WHERE cust_id=(SELECT cust_id FROM CUSTOMER WHERE cname LIKE "%kumar%");
+-- Query OK, 1 row affected (0.04 sec)
+SELECT * FROM ORDERS;
+/*
++----------+------------+---------+-----------+
+| order_id | odate      | cust_id | order_amt |
++----------+------------+---------+-----------+
+|     1001 | 2020-01-14 |       1 |      2000 |
+|     1002 | 2021-04-13 |       2 |       500 |
+|     1003 | 2019-10-02 |       3 |      2500 |
+|     1004 | 2019-05-12 |       5 |      1000 |
+|     1006 | 2021-12-12 |       3 |      5000 |
++----------+------------+---------+-----------+
+5 rows in set (0.00 sec)
+*/
+
+-- Find the item with the maximum unit price.
+SELECT MAX(unitprice) FROM ITEM;
+/*
++----------------+
+| MAX(unitprice) |
++----------------+
+|           1000 |
++----------------+
+1 row in set (0.00 sec)
+*/
+SELECT * FROM ITEM 
+WHERE unitprice=(SELECT MAX(unitprice) FROM ITEM);
+/*
++---------+-----------+
+| item_id | unitprice |
++---------+-----------+
+|   10003 |      1000 |
++---------+-----------+
+1 row in set (0.00 sec)
+*/
+SELECT item_id,unitprice AS max_unitprice 
+FROM ITEM 
+WHERE unitprice=(SELECT MAX(unitprice) FROM ITEM);
+/*
++---------+---------------+
+| item_id | max_unitprice |
++---------+---------------+
+|   10003 |          1000 |
++---------+---------------+
+1 row in set (0.00 sec)
+*/
+
+-- A trigger that updates order_amout based on quantity and unitprice of order_item
+DELIMITER $$
+CREATE TRIGGER UpdateOrderAmount
+AFTER INSERT ON ORDER_ITEM
+FOR EACH ROW
+BEGIN
+UPDATE ORDERS SET order_amt=(new.qty*(SELECT DISTINCT unitprice FROM ITEM NATURAL JOIN ORDER_ITEM WHERE item_id=new.item_id)) WHERE ORDERS.order_id=NEW.order_id;
+END; $$
+-- Query OK, 0 rows affected (0.06 sec)
+DELIMITER ;
+
+INSERT INTO ORDERS VALUES
+(1007,'2022-01-01',5,1300);
+-- Query OK, 1 row affected (0.06 sec)
+
+INSERT INTO ORDER_ITEM VALUES
+(1007,10005, 3);
+-- Query OK, 1 row affected (0.06 sec)
+
+SELECT *FROM ORDERS;
+/*
++----------+------------+---------+-----------+
+| order_id | odate      | cust_id | order_amt |
++----------+------------+---------+-----------+
+|     1001 | 2020-01-14 |       1 |      2000 |
+|     1002 | 2021-04-13 |       2 |       500 |
+|     1003 | 2019-10-02 |       3 |      2500 |
+|     1004 | 2019-05-12 |       5 |      1000 |
+|     1006 | 2021-12-12 |       3 |      5000 |
+|     1007 | 2022-01-01 |       5 |      1500 |
++----------+------------+---------+-----------+
+6 rows in set (0.00 sec)
+*/
+
+
+-- Create a view to display orderID and shipment date of all orders shipped from a warehouse
+CREATE VIEW OrderInfoOfWarehouse AS
+SELECT order_id,ship_date
+FROM SHIPMENT
+WHERE warehouse_id=502;
+-- Query OK, 0 rows affected (0.06 sec)
+SELECT * FROM OrderInfoOfWarehouse;
+/*
++----------+------------+
+| order_id | ship_date  |
++----------+------------+
+|     1002 | 2021-04-14 |
+|     1003 | 2019-10-07 |
++----------+------------+
+2 rows in set (0.00 sec)
+*/
